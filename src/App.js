95a99a12634ab2d17597406b0dbf00929c72b297
lequoctrang <quoctrang2701@gmail.com>
2022-05-30 10:44:07 +0700

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  return (
+    <div className="App">
+      <h1>{text}</h1>
+      <input onChange={(e) => setText(e.target.value)} />
+    </div>
+  );
+}
+
+export default App;
